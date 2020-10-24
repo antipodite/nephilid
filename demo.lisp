a31efc0ffed488987cb77719be7b2123dccd2cec
Isaac Stead <isaac.stead@protonmail.com>
2020-10-25 00:47:02 +1300

    Action remote calls returning reactions to client working.
    Next step is to allow specification of CSS selectors as arguments to
    functions defined with DEFACTION.

--- demo.lisp
+++ demo.lisp
@@ -5,9 +5,9 @@
 
 (nephilid::initialize-server "/nephilid")
 
-(defun change-text (selector)
-  (cl-json:encode-json-to-string `((selector . ,selector)
-                                   (content  . ,(format nil "crips")))))
+(nephilid::defaction test "#colour" ()
+  (with-html-string
+    (:p "Gamelan semar pegulingan")))
 
 (defroute root ("/" :acceptor http) ()
   (with-html-string
@@ -17,8 +17,10 @@
             (:script (nephilid::initialize-action-client "ws://localhost:1289/nephilid")))
      (:body
       (:p "hello nephilid")
+      (:span (:input :id "text"))
       (:span (:button :id "clickme"
-                      :onclick $(format t "sup G!!!")
+                      :onclick $(test)
                       "Click me"))
-      (:span (:button :id "colour"`
-                      "I should change colour :^)"))))))
+      (:span (:button :id "colour"
+                      "I should change colour :^)"))
+      (:p "Gamelan ensembles are found predominantly on the islands of Java and Bali and comprise mainly bronze metallophones and gongs. They are led by drummers (one in Javanese ensembles, two in Balinese ensembles) who play double-headed drums and give rhythmic signals to the other performers regarding tempo and dynamic changes.  Gamelan has spread internationally since the 1960s and today it is played in many ensembles associated with university music departments and embassies around the world. The Indonesian and international gamelan scene features both traditional and contemporary music. It is also strongly associated with the sophisticated puppetry and dance traditions of Indonesia.")))))
--- nephilid.lisp
+++ nephilid.lisp
@@ -3,10 +3,14 @@
 (setf cl-who:*attribute-quote-char* #\")
 
 (defparameter *resources* '())
-(defparameter *allowed-fns* (make-hash-table))
 
-(defun registered-p (symbol) t)
-  ;(gethash symbol *allowed-fns*))
+(defparameter *allowed-fns* (make-hash-table)
+  "Only functions that appear in this hash table are permitted to be
+evaluated by client remote calls. When a function is defined with DEFREACTION
+its naming symbol is registered here.")
+
+(defun registered-p (symbol)
+  (gethash symbol *allowed-fns*))
 
 (defclass nephilid-resource (hunchensocket:websocket-resource)
   ((route :initarg :route :initform (error "A route must be specified") :reader resource-route))
@@ -40,7 +44,17 @@ and a list of parameters to pass to the function."))
 (defgeneric serialize-action (action)
   (:documentation "This will be JSON most likely, but could be XML or something else.")
   (:method ((action nephilid-action))
-    (cl-json:encode-json-to-string action)))
+    (with-output-to-string (s-stream)
+      (flet ((print-symbol-with-package (symbol)
+               ;; Write string representation of symbol including package prefix
+               ;; Thanks to phoe on #lisp for this trick
+               (let ((*package* (find-package :keyword)))
+                 (write-string (str:concat "\"" (prin1-to-string symbol) "\"")
+                               s-stream))))
+        (with-accessors ((f action-function) (p action-params)) action
+          (json:with-object (s-stream)
+            (json:as-object-member ('function s-stream) (print-symbol-with-package f))
+            (json:encode-object-member 'params p s-stream)))))))
 
 (defun deserialize-action-json (string)
   "Convert a JSON string back into a nephilid-action"
@@ -58,18 +72,61 @@ and a list of parameters to pass to the function."))
           (apply f p)
           (error "Client tried to call unregistered function ~a" f)))))
 
+(defmacro defaction (name selector args &body body)
+  (setf (gethash name *allowed-fns*) t)
+  (let ((result (gensym)))
+    `(defun ,name ,args
+       (let ((,result ,@body))
+         (make-instance 'nephilid-reaction
+                        :selector ,selector
+                        :content ,result)))))
+
+;;; Allow calls to remote lisp functions to be embedded in generated HTML
+;;; as JSON objects that will be sent to the server to be invoked
+
+(defun remote-call-reader (stream char)
+  (declare (ignore char))
+  (format t "Current package: ~a~&" *package*)
+  (list (quote embed-action) (read stream t nil t)))
+
+(set-macro-character #\$ 'remote-call-reader)
+
+(defmacro embed-action (expr)
+  (let ((fn     (car expr))
+        (params (cdr expr)))
+    (unless (registered-p fn)
+      (error "~a is not a registered action. Embedded function calls must be ~
+              defined with DEFACTION" fn))
+    (alexandria:with-gensyms (json call)
+      `(let ((,json (serialize-action (make-instance 'nephilid-action
+                                                     :function ',fn
+                                                     :params ',params)))
+             ;; Need to interpolate json into JS method call like this
+             ;; so Parenscript doesn't re-escape string delimiters
+             (,call (ps:ps (ps:chain *nephilid-action-client*
+                                     (send "~a")))))
+         (format nil ,call ,json)))))
+
+;;;
+;;; Action functions called on the server send reactions to the client.
+;;;
+
 (defclass nephilid-reaction ()
   ((selector :initform nil
              :initarg :selector
              :accessor reaction-selector)
    (content :initform nil
             :initarg :content
-            :accessor reaction-content)))
+            :accessor reaction-content))
+  (:documentation "Holds the output and destination of a function call on the server.
+When the client receives a serialized nephilid-reaction, it looks at the selector and
+decides what to do with the content, for example replacing the innerHTML of element(s)
+indicated by the selector with the content."))
 
-(defgeneric serialize-reaction (reaction))
-
-(defmethod serialize-reaction ((reaction nephilid-reaction))
-  (cl-json:encode-json-to-string reaction))
+(defgeneric serialize-reaction (reaction)
+  (:documentation "Convert reaction to serialized format, usually JSON")
+  (:method ((reaction nephilid-reaction))
+    (cl-json:encode-json-to-string reaction)))
 
 (defun initialize-server (root-route)
   (let ((resource (make-instance 'nephilid-resource :route root-route)))
@@ -79,42 +136,44 @@ and a list of parameters to pass to the function."))
                      *resources* :test #'string= :key #'resource-route))
              hunchensocket:*websocket-dispatch-table*)))
 
-(defmethod hunchensocket:client-connected ((resource nephilid-resource) client)
-  (hunchensocket::send-message client "Nephilid 0.1.0"))
-
 (defmethod hunchensocket:text-message-received ((resource nephilid-resource) client message)
   "All messages sent from the client should be in the same format: "
-  (let ((result (handler-case
-                    (call-action-function (deserialize-action-json message))
-                  (json:json-syntax-error () nil))))
-    (when result
-      (hunchensocket::send-message client result))))
+  (let ((result (call-action-function (deserialize-action-json message))))
+    (if (eq (type-of result) 'nephilid-reaction)
+        (hunchensocket::send-message client (serialize-reaction result))
+        (error "Expected NEPHILID-REACTION, got ~a" (type-of result)))))
 
-(define-parenscript send-message (message)
-  (ps:chain *nephilid-action-client* (send (ps:lisp message))))
+(defmethod hunchensocket:client-connected ((resource nephilid-resource) client)
+  (hunchensocket::send-message client
+                               (serialize-reaction (make-instance 'nephilid-reaction
+                                                                  :content "Nephilid 0.1.0"
+                                                                  :selector nil))))
 
-;;; Allow calls to remote lisp functions to be embedded in generated HTML
-;;; as JSON objects that will be sent to the server to be invoked
+;;;
+;;; Client-side machinery
+;;;
 
-(defun remote-call-reader (stream char)
-  (list (quote build-remote-call) (read stream t nil t)))
+(defaction initial nil ()
+  "Nephilid 0.1.0")
 
-(set-macro-character #\$ 'remote-call-reader)
-
-(defmacro build-remote-call (expr)
-  (let ((fn (write-to-string (car expr)))
-        (params (cons 'ps:array (cdr expr))))
-    `(ps:ps
-       (ps:chain *nephilid-action-client*
-                 (send (ps:chain |json|
-                                 (stringify (ps:create "function" (ps:lisp ,fn)
-                                                       "params"   (ps:lisp ',params)))))))))
+(define-parenscript process-reaction ()
+  (ps:lambda (reaction)
+    (ps:let ((element  (ps:chain document (get-element-by-id (ps:@ reaction selector))))
+             (content  (ps:@ reaction content)))
+      (ps:setf (ps:@ element |innerHTML|) content))))
   
 (define-parenscript initialize-action-client (uri)
   (ps:defvar *nephilid-action-client* (ps:new (-web-socket (ps:lisp uri))))
   (ps:with-slots (onopen onerror onmessage) *nephilid-action-client*
-    (ps:setf onopen (ps:lambda () (ps:chain *nephilid-action-client* (send "Ping")))
-             onerror (ps:lambda (error) (ps:chain console (log (+ "Websocket error: " error))))
-             onmessage (ps:lambda (message) (ps:chain console (log (ps:@ message data)))))))
-
-
+    (ps:setf onerror (ps:lambda (error) (ps:chain console (log (+ "Websocket error: " error))))
+             onmessage (ps:lambda (reaction)
+                         (ps:chain console (log reaction))
+                         (ps:let* ((json     (ps:chain |json| (parse (ps:@ reaction data))))
+                                   (selector (ps:@ json selector))
+                                   (content  (ps:@ json content)))
+                           (if selector
+                               (ps:setf (ps:@ (ps:chain document
+                                                        (query-selector selector))
+                                              |innerHTML|)
+                                        content)
+                               (ps:chain console (log content))))))))
